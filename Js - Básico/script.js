de1db32527ac47fb38497878509876d5071afd66
Charllys <charllysbrauwolhaha@gmail.com>
2023-01-25 20:04:41 -0300

    JS Básico

---
+++ Js - Básico/script.js
@@ -0,0 +1,6 @@
+var nome = prompt("Qual seu nome?");
+var sobrenome = prompt("Qual seu sobrenome?");
+console.log("Olá " + nome + " " + sobrenome);
+
+document.write("<h1>Bem vindo "+ nome +" ao projeto!</h1>")
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto do Steve'/>")
