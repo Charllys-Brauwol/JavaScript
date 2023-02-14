12411b46ccbe777f58e5ad9d71b901e84eeafec4
Charllys <charllysbrauwolhaha@gmail.com>
2023-02-14 20:35:44 -0300

    js

---
+++ Js - Avançado/script.js
@@ -0,0 +1,49 @@
+//setInterval
+//Executa a função de tantos em tantos segundos
+/*function acao(){
+    document.write("Executando...<br/>")
+}
+setInterval(acao, 1000)
+setTimeout
+Executa a função somente uma vez 
+setTimeout(acao, 3000)*/
+
+
+
+
+//Diferença entre var, let e const
+/*
+var pode ser criada, acessada e alterada em qualquer lugar.
+let pode ser criada, acessada e alterada somente no escopo onde foi criada como um laço de repetição ou condicional
+const pode ser criada e acessada mas não alterar o valor dela, segue mesmo critério do let
+*/
+
+
+
+//Objetos
+/*let pessoa = {
+    nome: "Charllys",
+    idade: 26,
+    altura: 1.85, 
+    cargo: "Técniico"
+};
+
+let carro = {
+    nome: "Golf",
+    cor: "Branco"
+}
+
+let usuarios = [
+    {nome: "Charllys", cargo: "Técnico"}, {nome: "Carlos", cargo: "Suporte"}
+]
+
+console.log(usuarios);*/
+
+//Template Strings
+
+let nome = "Charllys";
+let sobrenome = "Brauwol";
+
+let mensagem = `Meu nome é ${nome} ${sobrenome}`;
+
+console.log(mensagem);
