aec2843b780a69db1f960b5a9c0ccddc7b91f599
Charllys <charllysbrauwolhaha@gmail.com>
2023-02-24 08:59:41 -0300

    Percorrendo lista de tarefa e rederizando na tela

--- Js - Avançado/script.js
+++ Js - Avançado/script.js
@@ -53,6 +53,18 @@ let inputElement = document.querySelector("#app input");
 let buttonElement = document.querySelector("#app button");
 let tarefas = [];
 
+function renderTarefas(){
+    listElement.innerHTML = '';
+
+    tarefas.map((todo)=> {
+        let liElement = document.createElement("li");
+        let tarefaText = document.createTextNode(todo);
+
+        liElement.appendChild(tarefaText);
+        listElement.appendChild(liElement);
+    })
+}
+
 function adicionarTarefas(){
     if(inputElement.value === ''){
         alert("Digite alguma tarefa");
@@ -62,6 +74,8 @@ function adicionarTarefas(){
 
         tarefas.push(novaTarefa);
         inputElement.value = '';
+
+        renderTarefas();
     }
 }
 
